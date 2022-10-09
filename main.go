0683603c3ac0d2f0ebd4f73d2ec4707f81a87d0e
updaun <updauney@daum.net>
2022-10-10 00:29:53 +0900

    for loop examples

--- main.go
+++ main.go
@@ -27,14 +27,41 @@ func lenAndUpper(name string) (length int, uppercase string) {
 	return
 }
 
+func superAdd(numbers ...int) int {
+	// for index, number := range numbers {
+	// 	fmt.Println(index, number)
+	// }
+	// return 1
+
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
+	// return 1
+
+	total := 0
+	for _, number := range numbers {
+		// total = total + number
+		total += number
+	}
+	return total
+}
+
 func main() {
 	// fmt.Println(multiply(2, 2))
 
-	totalLength, upperName := lenAndUpper("updaun")
-	fmt.Println(totalLength, upperName)
+	// totalLength, upperName := lenAndUpper("updaun")
+	// fmt.Println(totalLength, upperName)
 
 	// totalLength, _ := lenAndUpper("updaun")
 	// fmt.Println(totalLength)
 
 	// repeatMe("updaun", "python", "ai", "sql", "django")
+
+	// total := superAdd(1, 2, 3, 4, 5, 6)
+	// fmt.Println(total)
+
+	// superAdd(1, 2, 3, 4, 5, 6)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
+
 }
