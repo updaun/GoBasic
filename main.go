ea563eb1f301888bf6b52c9972ad058106195dfe
updaun <updauney@daum.net>
2022-10-10 00:20:05 +0900

    naked and defer

--- main.go
+++ main.go
@@ -10,21 +10,31 @@ func multiply(a, b int) int {
 	return a * b
 }
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
 
 func repeatMe(words ...string) {
 	fmt.Println(words)
 }
 
+// naked return
+func lenAndUpper(name string) (length int, uppercase string) {
+	// defer : 이 함수가 실행되고 나서 실행되는 부분
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
 func main() {
 	// fmt.Println(multiply(2, 2))
 
-	// totalLength, upperName := lenAndUpper("updaun")
-	// fmt.Println(totalLength, upperName)
+	totalLength, upperName := lenAndUpper("updaun")
+	fmt.Println(totalLength, upperName)
 
 	// totalLength, _ := lenAndUpper("updaun")
 	// fmt.Println(totalLength)
-	repeatMe("updaun", "python", "ai", "sql", "django")
+
+	// repeatMe("updaun", "python", "ai", "sql", "django")
 }
