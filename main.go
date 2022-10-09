6f41958509856be15b1159ff69abec1dbf454968
updaun <updauney@daum.net>
2022-10-09 22:46:49 +0900

    make function in go

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// func multiply(a int, b int) int {
+func multiply(a, b int) int {
+	return a * b
+}
+
 func main() {
-	fmt.Println("hello world!")
+	fmt.Println(multiply(2, 2))
 }
