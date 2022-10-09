b22ad807110265d0d7663b6c342edbec3624675b
updaun <updauney@daum.net>
2022-10-09 22:55:04 +0900

    function multiple output and input

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // func multiply(a int, b int) int {
 func multiply(a, b int) int {
 	return a * b
 }
 
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
-	fmt.Println(multiply(2, 2))
+	// fmt.Println(multiply(2, 2))
+
+	// totalLength, upperName := lenAndUpper("updaun")
+	// fmt.Println(totalLength, upperName)
+
+	// totalLength, _ := lenAndUpper("updaun")
+	// fmt.Println(totalLength)
+	repeatMe("updaun", "python", "ai", "sql", "django")
 }
