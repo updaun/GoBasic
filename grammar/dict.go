0e43c9a140a067a40db5b996509daf26725d09ce
updaun <updauney@daum.net>
2024-10-31 19:22:00 +0900

    chore: dictionary map

---
+++ grammar/dict.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"../grammar/mydict"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
+}
