142346fc2fd034afc37b5e7c80187115c16a3998
updaun <updauney@daum.net>
2024-11-01 22:16:23 +0900

    chore: dict add method

--- grammar/dict.go
+++ grammar/dict.go
@@ -8,9 +8,25 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	// definition, err := dictionary.Search("first")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
+
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
+	}
+	hello, err := dictionary.Search(word)
+	fmt.Println("found", word, "definition:", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	} else {
 		fmt.Println(definition)
 	}
