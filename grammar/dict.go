06f6d27ee8235518e7a99d026f5c29196e91f420
updaun <updauney@daum.net>
2024-11-02 20:05:40 +0900

    chore: update, delete method

--- grammar/dict.go
+++ grammar/dict.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
+	// dictionary := mydict.Dictionary{"first": "First word"}
 	// definition, err := dictionary.Search("first")
 	// if err != nil {
 	// 	fmt.Println(err)
@@ -15,19 +15,42 @@ func main() {
 	// 	fmt.Println(definition)
 	// }
 
-	word := "hello"
-	definition := "Greeting"
+	// 	word := "hello"
+	// 	definition := "Greeting"
 
-	err := dictionary.Add(word, definition)
+	// 	err := dictionary.Add(word, definition)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	hello, err := dictionary.Search(word)
+	// 	fmt.Println("found", word, "definition:", hello)
+	// 	err2 := dictionary.Add(word, definition)
+	// 	if err2 != nil {
+	// 		fmt.Println(err2)
+	// 	} else {
+	// 		fmt.Println(definition)
+	// 	}
+
+	// dictionary := mydict.Dictionary{}
+	// baseWord := "hello"
+	// dictionary.Add(baseWord, "First")
+	// err := dictionary.Update(baseWord, "Second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search(baseWord)
+	// fmt.Println(word)
+
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
-	}
-	hello, err := dictionary.Search(word)
-	fmt.Println("found", word, "definition:", hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
 	} else {
-		fmt.Println(definition)
+		fmt.Println(word)
 	}
+
 }
