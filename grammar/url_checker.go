42f7266a51331d13e66378c352895af10a8958cf
updaun <updauney@daum.net>
2024-11-04 20:17:42 +0900

    chore: add results in url checker

--- grammar/url_checker.go
+++ grammar/url_checker.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.naver.com",
 		"https://www.google.com",
@@ -22,14 +23,22 @@ func main() {
 		"https://www.sogou.com",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
 	}
 	return nil
