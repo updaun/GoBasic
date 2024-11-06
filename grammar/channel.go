1c027fe7279a9bd7e2c6cc88b4432de85d650a8f
updaun <updauney@daum.net>
2024-11-06 20:32:01 +0900

    chore: channel

---
+++ grammar/channel.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	// result := <-c
+	// fmt.Println(result)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
+
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
+}
