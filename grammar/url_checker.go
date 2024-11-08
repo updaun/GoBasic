3a2a68be7a8f153f6ff8caa0f7f3b98c69985772
updaun <updauney@daum.net>
2024-11-08 18:02:50 +0900

    chore: url checker Concurrent processing

--- grammar/url_checker.go
+++ grammar/url_checker.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.naver.com",
 		"https://www.google.com",
@@ -23,23 +29,22 @@ func main() {
 		"https://www.sogou.com",
 	}
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
