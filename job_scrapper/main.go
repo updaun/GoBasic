13a72c326bfb4dfe988d9e4684fdcba008eb79ed
updaun <updauney@daum.net>
2024-11-09 20:52:57 +0900

    chore: job scrapper init

---
+++ job_scrapper/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+
+func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+
+}
