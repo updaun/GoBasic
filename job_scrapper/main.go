e404e01927d1e97d041682d64e075bba9d55b2ff
updaun <updauney@daum.net>
2024-11-11 21:55:15 +0900

    chore: extract job part one

--- job_scrapper/main.go
+++ job_scrapper/main.go
@@ -9,12 +9,20 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
 	totalPages := getPages()
 
-	for i := 0; i < totalPages; i++ {
+	for i := 0; i < totalPages-8; i++ {
 		getPage(i)
 	}
 }
@@ -22,6 +30,24 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	searchCards := doc.Find(".item_recruit")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("value")
+		fmt.Println(id)
+		title := card.Find(".area_job>.job_tit>a").Text()
+		fmt.Println(title)
+		location := card.Find(".area_job>.job_condition>span>a").Text()
+		fmt.Println(location)
+	})
+
 }
 
 func getPages() int {
@@ -52,5 +78,4 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
-
 }
