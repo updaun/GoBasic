f21f5d886b3e800893f08c845b7cfca103a9cf1f
updaun <updauney@daum.net>
2024-11-16 16:07:28 +0900

    chore: go echo server

--- job_scrapper/main.go
+++ job_scrapper/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "myproject/scrapper"
+import (
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return nil
+}
 
 func main() {
-	scrapper.Scrape("python")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
